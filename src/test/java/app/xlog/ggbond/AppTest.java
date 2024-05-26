b2c5d7630ff4a4bfc3b64c7f9c8ddab5edfd895d
123 <1962883041@qq.com>
2024-05-27 00:45:20 +0800

    签名算法，测试接口

--- src/test/java/app/xlog/ggbond/AppTest.java
+++ src/test/java/app/xlog/ggbond/AppTest.java
@@ -1,38 +1,57 @@
 package app.xlog.ggbond;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import app.xlog.ggbond.utils.SignUtils;
+import okhttp3.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+
+@SpringBootTest
+public class AppTest {
+    private final OkHttpClient client = new OkHttpClient();
+
+    @Test
+    public void testSign() {
+        // 获取时间戳
+        long timestamp = System.currentTimeMillis() / 1000;
+        System.out.println(timestamp);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("mch_id", "1673424392");
+        params.put("out_trade_no", "zsbz20240526");
+        params.put("total_fee", "0.01");
+        params.put("body", "测试一下");
+        params.put("timestamp", String.valueOf(timestamp));
+        params.put("notify_url", "https://www.weixin.qq.com/wxpay/pay.php");
+
+        String sign = SignUtils.createSign(params, "6d3e889f359fcb83d150e9553a9217b9");
+        System.out.println(sign);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @Test
+    public void testScanCodePayment() throws IOException {
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/x-www-form-urlencoded"),
+                "mch_id=1673424392&out_trade_no=zsbz20240526&total_fee=0.01&body=测试一下&timestamp=1716741796&notify_url=https://www.weixin.qq.com/wxpay/pay.php&sign=8D25F57F499E8BF52E5805D8CA86DAF1"
+        );
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        Request request = new Request.Builder()
+                .url("https://api.ltzf.cn/api/wxpay/native")
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            System.out.println(response.body().string());
+        }
     }
 }
